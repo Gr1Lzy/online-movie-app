5e16df870a61b092730eee84b0432b4aa4a42a7b
Andrii Kolomoiets <kolomoets02@gmail.com>
2024-07-02 16:59:09 +0300

    add new env for backend

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   // Services
   USERS_SERVICE: 'http://localhost:3000',
-  MOVIE_SERVICE: 'http://localhost:8081',
+  MOVIE_SERVICE: process.env.BACKEND_MOVIE_SERVICE,
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
   getMovieService() {
     return this.MOVIE_SERVICE;
